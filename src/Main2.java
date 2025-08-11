1d2f6499759bd705742787972143dbf94c1366b8
Dilya test <dilya.yusufova@gmail.com>
2025-08-12 01:25:34 +0500

    Задание 2: расчет Bmi при помощи сервиса

---
+++ src/Main2.java
@@ -0,0 +1,9 @@
+public class Main2 {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double weight = 98;
+        double height = 1.87;
+        int bmi = service.calculate(weight, height);
+        System.out.println("Индекс массы тела: " + bmi);
+    }
+}
